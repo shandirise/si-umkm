95f6b20e707ab57632c0b7506e0d0eab56f83e97
shandirise <152406922+shandirise@users.noreply.github.com>
2025-06-25 09:32:37 +0700

    cek eror

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-// eslint.config.mjs
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -12,18 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // Nonaktifkan aturan 'no-explicit-any' untuk sementara
-      "@typescript-eslint/no-explicit-any": "off",
-      // Nonaktifkan aturan 'no-unused-vars' untuk sementara
-      "@typescript-eslint/no-unused-vars": "off",
-      // Nonaktifkan aturan 'prefer-const' untuk sementara
-      "prefer-const": "off",
-      // Nonaktifkan peringatan penggunaan tag <img> (jika Anda tidak ingin menggunakan next/image sekarang)
-      "@next/next/no-img-element": "off",
-    },
-  },
 ];
 
 export default eslintConfig;
